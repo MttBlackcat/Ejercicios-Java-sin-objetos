c2a32cc9974728c879cd4703b630a665e5ea3404
Javier Muñoz Vaquerizo <gothmitternacht@gmail.com>
2020-12-12 14:35:17 +0100

    BlackcatServingWeb

---
+++ blackcatServingWeb/src/main/java/com/blackcatservingweb/blackcatservingweb/BlackcatServingWebApplication.java
@@ -0,0 +1,13 @@
+package com.blackcatservingweb.blackcatservingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackcatServingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackcatServingWebApplication.class, args);
+	}
+
+}
